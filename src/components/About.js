e3ef851cdacb78d7b52269565a684fa5c5da8d34
RAJAT YADAV <rajatyadavindia2@gmail.com>
2023-10-02 19:14:11 +0530

    Update: Add new feature X

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,21 @@
 const About=()=>{
     return(
-        <div><h1>btaa acha apne baare me</h1></div>
+        <div className="flex justify-center items-center m-10">
+        <div className="flex justify-center w-8/12 items-center bg-gray-500">
+
+          <img className="w-3/12 m-2 rounded-2xl" src="https://i.ibb.co/1zL08Md/Grub-Hub.jpg"/>
+          <div className="m-3 p-3">
+          <h1 className="text-2xl">FRONT END DEVELOPER</h1>
+          <h1>Name: rajat yadav</h1>
+          <h1>Email: rajatyadavindia2@gmail.com</h1>
+          <h1>Github: https://github.com/rajatyadav1998</h1>
+          <h1>Experience: wipro(2 years)</h1>
+          </div>
+          
+
+          
+        </div>
+        </div>
     )
 
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,16 +16,17 @@ const Header=()=>{
     return (
         <div className="flex justify-between bg-pink-100 shadow-lg">
         <div className="w-24 p-1 m-2">
-        <img  className="rounded-lg" 
+        <img  className="rounded-lg " 
             src={LOGO_URL}alt="img"/>
         </div>
+        <div className="m-7 font-bold text-4xl bg-pink-300 p-2 rounded-xl"><i>FOOD GRID</i></div>
             <div className="flex items-center "> 
              <ul className="flex">
              
             <Link to="/"><li className="m-3">Home</li></Link>
             <Link to="/about"><li className="m-3">About</li></Link>
-            <Link to="/contact"><li className="m-3">Contact-us</li></Link>
-            <Link to="/cart"><li className="m-2 font-bold text-2xl">cart ({cartItems.length})</li></Link>
+            {/* <Link to="/contact"><li className="m-3">Contact-us</li></Link> */}
+            <Link to="/cart"><li className="m-2 font-bold text-2xl ">🛒 ({cartItems.length})</li></Link>
                 
                 <button className="px-6" onClick={()=>{
                     btnName==="login"
